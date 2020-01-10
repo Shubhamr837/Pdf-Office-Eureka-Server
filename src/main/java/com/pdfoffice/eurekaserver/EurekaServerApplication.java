bbabc325fb2bd944b353c4d054f79bad2ef33068
shubhamr837 <sremail2010@gmail.com>
2020-01-10 20:00:36 +0530

    Aws configuration

--- src/main/java/com/pdfoffice/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/pdfoffice/eurekaserver/EurekaServerApplication.java
@@ -3,13 +3,21 @@ package com.pdfoffice.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
+	@RequestMapping(value = "/")
+	public ResponseEntity healthcheck(){
+		return ResponseEntity.ok().build();
+	}
 }
